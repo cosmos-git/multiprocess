1d0bd0931022d4d39128c9817e03c18b5131510e
Jagannathan Raman <jag.raman@oracle.com>
2018-11-28 10:36:27 -0500

    multi-process: create IOHUB object to handle irq
    IOHUB object is added to manage PCI IRQs. It uses KVM_IRQFD
    ioctl to create irqfd to injecting PCI interrupts to the guest.
    IOHUB object forwards the irqfd to the remote process. Remote process
    uses this fd to directly send interrupts to the guest, bypassing QEMU.
    
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/remote/mpqemu-link.c
+++ hw/remote/mpqemu-link.c
@@ -253,6 +253,11 @@ bool mpqemu_msg_valid(MPQemuMsg *msg)
             return false;
         }
         break;
+    case MPQEMU_CMD_SET_IRQFD:
+        if (msg->size || (msg->num_fds != 2)) {
+            return false;
+        }
+        break;
     default:
         break;
     }
